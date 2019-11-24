1b5737893816962e06495e99153363da03c62333
kondors1995 <normandija1945@gmail.com>
2019-11-24 16:44:51 +0000

    raphael: Official syberia

--- syberia_raphael.mk
+++ syberia_raphael.mk
@@ -17,6 +17,8 @@ PRODUCT_DEVICE := raphael
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_NAME := syberia_raphael
 
+SYBERIA_BUILD_TYPE := OFFICIAL
+
 BUILD_FINGERPRINT := "Xiaomi/raphaelin/raphaelin:9/PKQ1.181121.001/V10.3.3.0.PFKINXM:user/release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
