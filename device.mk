3f53bd0ea84ea6c175490812801f1a3693c8d555
DennySPB <dennyspb@gmail.com>
2020-01-17 17:34:37 +0300

    Include google/vendor/customization
    for google play apex updates
    
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    Change-Id: Ie7822add773a372a2df974e003d11380821c2c60

--- device.mk
+++ device.mk
@@ -9,6 +9,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+$(call inherit-product, vendor/google/customization/config.mk)
 
 PRODUCT_CHARACTERISTICS := nosdcard
 
