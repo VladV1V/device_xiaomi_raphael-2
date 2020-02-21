c0a2063dd0afa526223f3cd07f1ff3c894d379e8
kondors1995 <normandija1945@gmail.com>
2020-02-21 10:21:08 +0000

    Revert "raphael: Track and include customized version of ANX Camera"
    This reverts commit 0417bbabc01e0264f38a1a7a564d435f64c5f783.

--- syberia_raphael.mk
+++ syberia_raphael.mk
@@ -10,7 +10,6 @@ $(call inherit-product, device/xiaomi/raphael/device.mk)
 $(call inherit-product, vendor/syberia/common.mk)
 
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_SUPPORTS_GOOGLE_RECORDER := false
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_BRAND := Xiaomi
@@ -23,6 +22,3 @@ SYBERIA_BUILD_TYPE := OFFICIAL
 BUILD_FINGERPRINT := "Xiaomi/raphaelin/raphaelin:9/PKQ1.181121.001/V10.3.3.0.PFKINXM:user/release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-# Inherit from custom vendor
-$(call inherit-product, vendor/MiuiCamera/config.mk)
