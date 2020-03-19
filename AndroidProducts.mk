cmorlok/android_device_xiaomi_raphael

raphael: Switch from add_lunch_combo to COMMON_LUNCH_CHOICES
Bug: 77599627
Test: lunch  [see entries in list]
Change-Id: Id44fcfec47eeb979a745f879031f1bec7359a633

AndroidProducts.mk
@@ -6,3 +6,8 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_raphael.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_raphael-user \
+    lineage_raphael-userdebug \
+    lineage_raphael-eng

VladV1V/device_xiaomi_raphael-2

Signed-off-by: UtsavisGreat <utsavbalar1231@gmail.com>

AndroidProducts.mk
@@ -14,3 +14,8 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/syberia_raphael.mk
+
+COMMON_LUNCH_CHOICES := \
+    syberia_raphael-user \
+    syberia_raphael-userdebug \
+    syberia_raphael-eng